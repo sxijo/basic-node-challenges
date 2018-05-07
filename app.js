30dc0bb1a8285030af5c46f8b68eec0ca5c30816
sxijo <36853319+sxijo@users.noreply.github.com>
2018-05-07 10:34:25 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+  console.log('You requested', req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello World');
+})
+
+server.listen(3000, '127.0.0.1')
+console.log('listening on port 3000...');
